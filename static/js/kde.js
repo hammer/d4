41fc035c08f041a4b4e46ff5d0e6642f90ce4573
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2012-02-20 22:31:39 -0800

    Add a scale to kde.

--- static/js/kde.js
+++ static/js/kde.js
@@ -2,9 +2,10 @@ d3.csv("f500.csv", function handleCSV(csv) {
   // select revenues of CA companies
   var data = csv.filter(function(el) { return el.state_location == "CA"; })
                 .map(function(el) { return parseInt(el.revenues) });
-  var w = 275,
-      h = 225,
-      xmin = d3.min(data),
+  var p = 20,
+      w = 275 - 2 * p,
+      h = 225 - 2 * p,
+      xmin = 0,
       xmax = d3.max(data),
       x = d3.scale.linear()
                   .domain([xmin, xmax])
@@ -19,14 +20,37 @@ d3.csv("f500.csv", function handleCSV(csv) {
                   .range([0, h]);
 
   var vis = d3.select("div#kde")
-       	      .append("svg")
-	      .attr("width", w)
-              .attr("height", h);
+              .append("svg")
+              .attr("width", w + 2 * p)
+              .attr("height", h + 2 * p)
+              .append("g")
+              .attr("transform", "translate(" + p + "," + p + ")");        
 
+  // density plot
   var line = d3.svg.line()
   	           .x(function(d) { return x(d[0]); })
 	           .y(function(d) { return h - y(d[1]); });
 
   vis.append("path")
      .attr("d", line(kde));
+
+  // x axis
+  var xrule = vis.selectAll("div#kde g.x")
+                 .data(x.ticks(5))
+                 .enter()
+                 .append("g")
+                 .attr("class", "x");
+
+  xrule.append("line")
+       .attr("x1", x)
+       .attr("x2", x)
+       .attr("y1", 0)
+       .attr("y2", h);
+
+  xrule.append("text")
+       .attr("x", x)
+       .attr("y", h + 3)
+       .attr("dy", ".71em")
+       .attr("text-anchor", "middle")
+       .text(x.tickFormat(5));
 });

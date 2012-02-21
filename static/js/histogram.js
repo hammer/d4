086bc2828006c0ec6bed9f9fdf0d8358c7cbf80b
Jeff Hammerbacher <jeff.hammerbacher@gmail.com>
2012-02-20 22:28:41 -0800

    Moved to linear scale. Will migrate to axis framework next.

--- static/js/histogram.js
+++ static/js/histogram.js
@@ -9,14 +9,9 @@ d3.csv("f500.csv", function handleCSV(csv) {
       w = 275 - 2 * p;
       h = 225 - 2 * p;
  
-  var x = d3.scale.ordinal()
-                  .domain(histogram.map(function(d) { return d.x; }))
-                  .rangeRoundBands([0, w]);
-
-  //  var x = d3.scale.linear()
-  //                  .domain([d3.min(histogram.map(function(d) { return d.x; })),
-  //                           d3.max(histogram.map(function(d) { return d.x+d.dx; }))])
-  //                  .range([0, width]);
+  var x = d3.scale.linear()
+                  .domain([0, d3.max(data)])
+                  .range([0, w]);
  
   var y = d3.scale.linear()
                   .domain([0, d3.max(histogram.map(function(d) { return d.y; }))])
@@ -36,16 +31,12 @@ d3.csv("f500.csv", function handleCSV(csv) {
      .append("rect")
      .attr("x", function(d) { return x(d.x); })
      .attr("y", function(d) { return h - y(d.y); })
-     .attr("width", x.rangeBand()) // function(d) { return x(d.dx); }
+     .attr("width", function(d) { return x(d.dx); })
      .attr("height", function(d) { return y(d.y); });
  
-  // x axis (ordinal axes can't configure number of ticks)
-  var ticks = x.domain().filter(function(d, i) { return i % 8 == 5; });
-  console.log(ticks);
-
   // x axis
   var xrule = vis.selectAll("div#histogram g.x")
-                 .data(ticks)
+                 .data(x.ticks(5))
                  .enter()
                  .append("g")
                  .attr("class", "x");
@@ -61,5 +52,5 @@ d3.csv("f500.csv", function handleCSV(csv) {
        .attr("y", h + 3)
        .attr("dy", ".71em")
        .attr("text-anchor", "middle")
-      .text(d3.format(",.0f"));
+       .text(x.tickFormat(5));
 });
